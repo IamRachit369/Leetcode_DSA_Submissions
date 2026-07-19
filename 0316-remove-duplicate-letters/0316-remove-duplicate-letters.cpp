97d92a89c5decdff9ba7e2564b9af91faddf5f8a
Rachit Saini <rachit.college.mnnit@gmail.com>
2026-07-19 22:58:30 +0530

    Time: 0 ms (100%), Space: 9.7 MB (17.91%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool>mp(26, false);
+        int n = s.size();
+        stack<char>st;
+        unordered_map<char,int>last;
+        for(int i = 0; i < n; i++)
+        {
+            last[s[i]] = i;
+        }
+        // string curr = "";
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(!mp[s[i]-'a'])
+            {
+                while(!st.empty() && (st.top() > s[i]) && last[st.top()] > i)
+                {
+                    // curr.erase(curr.size() - 1, 1);
+                    mp[st.top()-'a'] = false;
+                    st.pop();
+                }
+                // curr += s[i];
+                st.push(s[i]);
+                mp[s[i]- 'a'] = true;
+            }
+            // cout << curr << endl;
+        }
+        string ans = "";
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
