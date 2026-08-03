7097684963ce11ddb544c1008f7dcb9b946f0981
Rachit Saini <rachit.college.mnnit@gmail.com>
2026-08-04 02:30:17 +0530

    Time: 32 ms (55.5%), Space: 140.9 MB (49.01%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
+    vector<int>dp;
+    int recur(int l, vector<int>&nums)
+    {
+        if(l >= nums.size())
+        {
+            return 0;
+        }
+        if(dp[l] != INT_MIN)
+        {
+            return dp[l];
+        }
+        int count = INT_MIN;
+        int temp = 0;
+        for(int i = 0; i < 3; i++)
+        {
+            if(l + i >= nums.size())
+            {
+                break;
+            }
+            temp += nums[l + i];
+            count = max(count, temp - recur(l + i + 1, nums));
+        }
+        return dp[l] = count;
+    }
     string stoneGameIII(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n + 3, 0);
-        for(int l = n - 1; l >= 0; l--)
+        dp.resize(n, INT_MIN);
+        int ans = recur(0, nums);
+        cout << ans << endl;
+        if(ans > 0)
         {
-            int temp = 0;
-            dp[l] = INT_MIN;
-
-            for(int i = 0; i < 3 && l + i < n; i++)
-            {
-                temp += nums[l + i];
-
-                dp[l] = max(dp[l], temp - dp[l + i + 1]);
-            }
-        }
-        if(dp[0] > 0)
             return "Alice";
-        else if(dp[0] < 0)
+        }
+        else if(ans < 0)
+        {
             return "Bob";
+        }
         return "Tie";
     }
 };
