f3925c9e76eff7f0f8211f3316f311e827c8d10b
Rachit Saini <rachit.college.mnnit@gmail.com>
2026-08-05 10:09:40 +0530

    Time: 3 ms (40.68%), Space: 13 MB (15.74%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // basically find NGE of nums2
+        stack<int> st;
+        vector<int> NGE(nums2.size(), -1);
+
+        for(int i = nums2.size() - 1; i >= 0; i--)
+        {
+            while(!st.empty() && st.top() <= nums2[i])
+            {
+                st.pop();
+            }
+
+            if(!st.empty())
+            {
+                NGE[i] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums2.size(); i++)
+        {
+            mp[nums2[i]] = NGE[i];
+        }
+
+        vector<int> ans;
+
+        for(int x : nums1)
+        {
+            ans.push_back(mp[x]);
+        }
+
+        return ans;
+    }
+};
