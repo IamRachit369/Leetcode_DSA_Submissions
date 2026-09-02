6507c4d250388e2ab689c212a053ba7f1262825d
Rachit Saini <rachit.college.mnnit@gmail.com>
2026-09-02 22:32:44 +0530

    Time: 0 ms (100%), Space: 10.4 MB (66.16%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    int dp[501][501];
-    int recur(vector<int>&piles, int l, int r)
-    {
-        if(l == r)
-        {
-            return piles[l];
-        }
-        if(dp[l][r] != -1){
-            return dp[l][r];
-        }
-        int left = piles[l] - recur(piles, l + 1, r);
-        int right = piles[r] - recur(piles, l, r-1);
-        return dp[l][r] = max(left, right);
-    }
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        memset(dp, -1, sizeof(dp));
-        return recur(piles, 0, n-1) > 0;
+        return true;
     }
 };
